a14f1c680c21ffd408786519bdb22a2ce632561d
Amanda Gilmore <ms.amgilmore@gmail.com>
2015-01-12 20:25:19 -0800

    changed to pull Monday sales

--- Homework01/process.py
+++ Homework01/process.py
@@ -3,7 +3,7 @@ def main():
     for line in my_file:
         line = line.rstrip()
         day = line[0:3]
-        if day == "Tue":
+        if day == "Mon":
             print line
 
 if __name__ == "__main__":
